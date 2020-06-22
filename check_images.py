71e247c8d525730a2d8e9e91f3cec76db5056eb0
Harshit Chari <55452858+HarshitChari@users.noreply.github.com>
2020-06-22 23:40:07 +0530

    Update check_images.py

--- check_images.py
+++ check_images.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-# */AIPND-revision/intropyproject-classify-pet-images/check_images.py
-#
-# TODO 0: Add your information below for Programmer & Date Created.
-# PROGRAMMER: Duong Nguyen
-# DATE CREATED: 12/22/18
-# REVISED DATE:
-# PURPOSE: Classifies pet images using a pretrained CNN model, compares these
-#          classifications to the true identity of the pets in the images, and
-#          summarizes how well the CNN performed on the image classification task.
-#          Note that the true identity of the pet (or object) in the image is
-#          indicated by the filename of the image. Therefore, your program must
-#          first extract the pet image label from the filename before
-#          classifying the images using the pretrained CNN model. With this
-#          program we will be comparing the performance of 3 different CNN model
-#          architectures to determine which provides the 'best' classification.
-#
-# Use argparse Expected Call with <> indicating expected user input:
-#      python check_images.py --dir <directory with images> --arch <model>
-#             --dogfile <file that contains dognames>
-#   Example call:
-#    python check_images_solution.py --dir pet_images/ --arch vgg --dogfile dognames.txt
-# #
 
 # Imports python modules
 from time import time, sleep
