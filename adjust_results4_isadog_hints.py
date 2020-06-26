c572a503e5d04a6330b1fa887499749bc4f89c53
Harshit Chari <55452858+HarshitChari@users.noreply.github.com>
2020-06-26 13:32:45 +0530

    Update adjust_results4_isadog_hints.py

--- adjust_results4_isadog_hints.py
+++ adjust_results4_isadog_hints.py
@@ -1,12 +1,6 @@
 #            -The results dictionary as results_dic within adjust_results4_isadog 
 #             function and results for the function call within main.
 #            -The text file with dog names as dogfile within adjust_results4_isadog
-#             function and in_arg.dogfile for the function call within main. 
-#           This function uses the extend function to add items to the list 
-#           that's the 'value' of the results dictionary. You will be adding the
-
-#           3 of the list and whether or not the classifier label is of-a-dog as
-#           the item at index 4 of the list. Note we recommend setting the values
 #           at indices 3 & 4 to 1 when the label is of-a-dog and to 0 when the 
 #           label isn't a dog.
 #
