3762d38b4f8eae312e61b9bbb5cf4e3abcd5ff91
Harshit Chari <55452858+HarshitChari@users.noreply.github.com>
2020-07-03 22:33:53 +0530

    Update classify_images.py

--- classify_images.py
+++ classify_images.py
@@ -1,22 +1,5 @@
-#            -The results dictionary as results_dic within classify_images 
-#             function and results for the functin call within main.
-#            -The CNN model architecture as model within classify_images function
-#             and in_arg.arch for the function call within main. 
-#           This function uses the extend function to add items to the list 
-#           that's the 'value' of the results dictionary. You will be adding the
-#           classifier label as the item at index 1 of the list and the comparison 
-#           of the pet and classifier labels as the item at index 2 of the list.
-#
-# #
-# Imports classifier function for using CNN to classify images 
 from classifier import classifier 
-
-
-# TODO 3: Define classify_images function below, specifically replace the None
-#       below by the function definition of the classify_images function. 
-#       Notice that this function doesn't return anything because the 
-#       results_dic dictionary that is passed into the function is a mutable 
-#       data type so no return is needed.
+data type so no return is needed.
 # 
 def classify_images(images_dir, results_dic, model):
     """
